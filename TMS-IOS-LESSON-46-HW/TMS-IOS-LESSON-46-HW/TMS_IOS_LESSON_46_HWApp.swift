779a5ded83cfa7145527db69f0f37513df40f4b1
mac <mac@macs-MacBook-Pro.local>
2024-05-28 21:34:03 +0300

    init

---
+++ TMS-IOS-LESSON-46-HW/TMS-IOS-LESSON-46-HW/TMS_IOS_LESSON_46_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMS_IOS_LESSON_46_HWApp.swift
+//  TMS-IOS-LESSON-46-HW
+//
+//  Created by mac on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TMS_IOS_LESSON_46_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
